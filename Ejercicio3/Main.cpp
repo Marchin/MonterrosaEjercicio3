3cc44132685109792da1d313d6fd369475bf5c48
Marchin <mmonterrosa94@gmail.com>
2018-05-15 16:52:25 -0300

    Added Recatangles for both players
    Added Recatangles for both players
    Added Rectangle's size macros

--- Ejercicio3/Main.cpp
+++ Ejercicio3/Main.cpp
@@ -3,10 +3,16 @@
 #include <allegro5/allegro_primitives.h>
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
+#define REC_WIDTH 15
+#define REC_HEIGHT 60
 #define WHITE al_map_rgb(255, 255, 255)
 
 int main(int argc, char **argv) {
 	ALLEGRO_DISPLAY *display = NULL;
+	float posP1_x = 0;
+	float posP1_y = 0;
+	float posP2_x = SCREEN_WIDTH - REC_WIDTH;
+	float posP2_y = 0;
 
 	if (!al_init()) {
 		fprintf(stderr, "failed to initialize allegro!\n");
@@ -23,7 +29,12 @@ int main(int argc, char **argv) {
 
 	//al_clear_to_color(al_map_rgb(0, 0, 0));
 
-	al_draw_filled_rectangle(0, 0, 15,60, WHITE);
+	al_draw_filled_rectangle(posP1_x, posP1_y, 
+		posP1_x + REC_WIDTH, posP1_y + REC_HEIGHT, WHITE);
+
+
+	al_draw_filled_rectangle(posP2_x, posP2_y, 
+		posP2_x + REC_WIDTH, posP2_y + REC_HEIGHT, WHITE);
 
 	al_flip_display();
 
