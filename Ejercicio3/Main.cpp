b53c49ff29aacead41df2bf9dc75b0d1fe5d73e0
Marchin <mmonterrosa94@gmail.com>
2018-05-15 16:39:57 -0300

    Added primitives
    Added allegro_primitives.h.
    A white rectangle is drawn as a test

--- Ejercicio3/Main.cpp
+++ Ejercicio3/Main.cpp
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_primitives.h>
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+#define WHITE al_map_rgb(255, 255, 255)
 
 int main(int argc, char **argv) {
-	ALLEGRO_COLOR white = al_map_rgb(255, 255, 255);
 	ALLEGRO_DISPLAY *display = NULL;
 
 	if (!al_init()) {
@@ -11,13 +13,17 @@ int main(int argc, char **argv) {
 		return -1;
 	}
 
-	display = al_create_display(640, 480);
+	display = al_create_display(SCREEN_WIDTH, SCREEN_HEIGHT);
 	if (!display) {
 		fprintf(stderr, "failed to create display!\n");
 		return -1;
 	}
 
-	al_clear_to_color(al_map_rgb(0, 0, 0));
+	al_init_primitives_addon();
+
+	//al_clear_to_color(al_map_rgb(0, 0, 0));
+
+	al_draw_filled_rectangle(0, 0, 15,60, WHITE);
 
 	al_flip_display();
 
